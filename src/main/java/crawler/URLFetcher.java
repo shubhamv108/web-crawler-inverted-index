7338e69d03d616595051ae47e71763e6600dbb9f
Shubham <16763337+shubham-v@users.noreply.github.com>
2021-10-13 15:00:43 +0530

    partital working solution

--- src/main/java/crawler/URLFetcher.java
+++ src/main/java/crawler/URLFetcher.java
@@ -1,6 +1,8 @@
 package crawler;
 
 import index.DocumentStore;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import utils.HttpClient;
 
 import java.net.URL;
@@ -24,7 +26,13 @@ public class URLFetcher {
             node = this.queue.fetch();
             System.out.println("Processing url: " + node.getUrl());
             String content = HttpClient.invoke(new URL(node.getUrl()));
-            Files.write(Paths.get("/home/shubham/git/rapyuto/output"), content.getBytes());
+//            Document html = Jsoup.parse(content);
+//            String title = html.title();
+//            String finalContent = title;
+//            for (int i = 1; i < 4; i++)
+//                finalContent  += (" " + html.body().getElementsByTag("h" + i).text());
+//
+//            Files.write(Paths.get("/home/shubham/git/rapyuto/output"), (content +"\n---\n"+ finalContent).getBytes());
             this.documentStore.create(node.getUrl(), content, node.depth);
         } catch (Exception ex) {
             System.out.println("Exception occured while processing url: " + node.getUrl());
